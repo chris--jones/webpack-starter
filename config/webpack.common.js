0bd4a0abefe576892aba1e47eb37ae0a6e9db7cb
Chris <zorlanv@gmail.com>
2018-02-18 09:53:43 +1100

    Angular upgrade & warning fixes
    Upgrade Angular to 5.2.5 and fix Webpack warnings - https://github.com/angular/angular/issues/11580

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -53,9 +53,8 @@ module.exports = {
     // Workaround for angular/angular#11580
     new webpack.ContextReplacementPlugin(
       // The (\\|\/) piece accounts for path separators in *nix and Windows
-      /angular(\\|\/)core(\\|\/)@angular/,
-      helpers.root('./src'), // location of your src
-      {} // a map of your routes
+      /(.+)?angular(\\|\/)core(.+)?/,
+      helpers.root('src') // location of your src
     ),
 
     new webpack.optimize.CommonsChunkPlugin({
